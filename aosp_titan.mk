8ffdfae98c84dd5cd26da8e1117dc68a048dd779
shubhmg <shubhmgajra2@gmail.com>
2016-05-26 03:52:17 +0530

    AOSP Bringup

--- aosp_titan.mk
+++ aosp_titan.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/motorola/titan/full_titan.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/aosp/common.mk)
 
 PRODUCT_RELEASE_NAME := titan
-PRODUCT_NAME := cm_titan
+PRODUCT_NAME := aosp_titan
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
